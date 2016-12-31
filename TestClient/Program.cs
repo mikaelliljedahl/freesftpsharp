96284d3617fa91d9e25c06d400868821dfad0974
0xFireball <0xFireball@outlook.com>
2016-12-31 10:53:46 -0800

    add client to run commands

---
+++ TestClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using Renci.SshNet;
+using System;
+using System.Net;
+
+namespace TestClient
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var testUsername = "test_person";
+            var testPassword = "1234";
+            var connInfo = new ConnectionInfo(IPAddress.Loopback.ToString(), 4022, testUsername,
+                new AuthenticationMethod[]
+                {
+                    // Password auth
+                    new PasswordAuthenticationMethod(testUsername, testPassword)
+                }
+            );
+            using (var sshClient = new SshClient(connInfo))
+            {
+                sshClient.Connect();
+                var commands = new[] { "info", "hello", "rm -rf /" };
+                foreach (var cmd in commands)
+                {
+                    using (var sshCmd = sshClient.CreateCommand(cmd))
+                    {
+                        sshCmd.Execute();
+                        var result = sshCmd.Result;
+                        Console.WriteLine($"exec: {cmd}, result: {result}");
+                    }
+                }
+            }
+        }
+    }
+}
